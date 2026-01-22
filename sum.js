a29534c2d7054bc6b5b06dfed081a9137ceea925
GabrielLopez <gmlopez11@espe.edu.ec>
2026-01-22 08:56:02 -0500

    Cambio realizado

--- sum.js
+++ sum.js
@@ -6,13 +6,25 @@ router.post('/sum',sumar);
 
 async function sumar(req,res) {
       try {
+        const {num1,num2} = req.body;
+        if(!num1){
+            res.status(400).json({
+            message:"Ingrese al menos 2 numeros para realizar la suma"
+            });
+        }
+        if(!num2){
+            res.status(400).json({
+            message:"Ingrese al menos 2 numeros para realizar la suma"
+            });
+        }
+    const suma = num1+num2;
     res.status(200).json({
-        suma:req.body.num1+req.body.num2
-    })
+        suma
+    });
     }catch(err){
     res.status(500).json({
             message:"No se pudo hacer la suma"
-        })
+        });
     }
 }
 module.exports = router;
